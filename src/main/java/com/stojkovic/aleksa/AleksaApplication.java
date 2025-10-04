70516064efa1f7aee19a74da3fdb4b59e847f81c
astojkovicc-web <astojkovicc017@gmail.com>
2025-10-04 18:26:49 +0200

    dodat spring

---
+++ src/main/java/com/stojkovic/aleksa/AleksaApplication.java
@@ -0,0 +1,13 @@
+package com.stojkovic.aleksa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleksaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AleksaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stojkovic/aleksa/AleksaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stojkovic.aleksa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleksaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
